d81efcb71dd740fe82e740c026ffeb41992a7dfc
Song0308-code <songseungwoo52@gmail.com>
2025-12-17 20:01:12 +0900

    commit
    commit

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    [Header("UI ¿¬°á")]
+    [Header("UI")]
     public TextMeshProUGUI timeText;   
     public TextMeshProUGUI bestScoreText;  
 
