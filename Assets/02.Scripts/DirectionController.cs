ce32e87a8cc0c87e5fbe87df7ae7bba2c3a205f3
bluem3648 <bluem3648@gmail.com>
2025-12-18 00:37:24 +0900

    0036
    PauseMenu, Button redesign

--- Assets/02.Scripts/DirectionController.cs
+++ Assets/02.Scripts/DirectionController.cs
@@ -5,6 +5,8 @@ public class DirectionController : MonoBehaviour
 {
     private void Update()
     {
+        if (Time.timeScale == 0) return;
+
         Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
         Vector3 screenPos = new Vector3(mouseScreenPos.x, mouseScreenPos.y, Mathf.Abs(Camera.main.transform.position.z - transform.position.z));
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(screenPos);
